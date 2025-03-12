04fb4f609172df34fd7373c08ab79aab2c932ade
Tran Hoang Khiem <sngkhang146@gmail.com>
2025-03-12 21:33:41 +0700

    Delete publication page

--- src/lib/config/general.ts
+++ src/lib/config/general.ts
@@ -42,11 +42,7 @@ export const header: HeaderConfig = {
     {
       link: '/about-me',
       text: 'About'
-    },
-    {
-      link: '/publications',
-      text: 'Publications'
-    },
+    }
   ],
   search: {
     colors: true,
